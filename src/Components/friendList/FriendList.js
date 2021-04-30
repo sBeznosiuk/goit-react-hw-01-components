7b9a1661f6645277cdade2c7f70981571f3e10ce
serhii beznosiuk <ostem971@gmail.com>
2021-04-30 03:51:33 +0300

    01-main

---
+++ src/Components/friendList/FriendList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FriendList = ({ friends }) => (
+  <ul className="friend-list">
+    {friends.map((friend) => (
+      <li className={friend.item} key={friend.id}>
+        <span className="status">{friend.isOnline}</span>
+        <img
+          className="avatar"
+          src={friend.avatar}
+          alt={friend.name}
+          width="48"
+        />
+        <p className="name">{friend.name}</p>
+      </li>
+    ))}
+  </ul>
+);
+
+export default FriendList;
---
+++ src/Components/profile/Profile.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import preview from "./social-profile/preview.png";
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className="avatar"
+          width="120"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">{`@${tag}`}</p>
+        <p className="location">{location}</p>
+        {renderStats(stats)}
+      </div>
+    </div>
+  );
+};
+
+function renderStats(stats) {
+  const statsKeys = Object.keys(stats);
+  return (
+    <ul className="stats">
+      {statsKeys.map((item) => {
+        return (
+          <li key={item}>
+            <span className="label">{item}</span>
+            <span className="quantity">{stats[item]}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+Profile.defaultProps = {
+  avatar: preview,
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
+
+export default Profile;
